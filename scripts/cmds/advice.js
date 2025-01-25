4f1b92c09157e44dfb431fd06ff1a30df083ef50
AbdulHuun <mumtajxakkaho@gmail.com>
2025-01-26 00:35:17 +0545

    Add advice.js

---
+++ scripts/cmds/advice.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "advice",
+    version: "1.0.0",
+    aliases: ["getAdvice"],
+    author: "YourName",
+    role: 0,
+    category: "Utility",
+    description: "Fetches a random piece of advice.",
+    guide: {
+      vi: "Lấy một lời khuyên ngẫu nhiên.",
+      en: "Fetches a random piece of advice."
+    }
+  },
+
+  onStart: async function ({ bot, message, args }) {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      const advice = response.data.slip.advice;
+      message.reply(advice);
+    } catch (error) {
+      message.reply("Sorry, I couldn't fetch advice at the moment.");
+    }
+  }
+};
